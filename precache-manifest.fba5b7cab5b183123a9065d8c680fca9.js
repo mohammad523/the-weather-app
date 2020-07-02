8fc407e129ec703478ae4cdf46212238c1722a5c
Mohammad Hasan <its_mh@outlook.com>
2020-07-02 16:15:03 -0500

    Updates

---
+++ precache-manifest.fba5b7cab5b183123a9065d8c680fca9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441b2bc60133470cfbaa8b5277d19883",
+    "url": "/mohammad523/the-weather-app/tree/master/morningdose/index.html"
+  },
+  {
+    "revision": "0f2279a3e7350efc6f27",
+    "url": "/mohammad523/the-weather-app/tree/master/morningdose/static/css/main.9f68dd81.chunk.css"
+  },
+  {
+    "revision": "999f656973cda5f356c4",
+    "url": "/mohammad523/the-weather-app/tree/master/morningdose/static/js/2.3f22fb41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mohammad523/the-weather-app/tree/master/morningdose/static/js/2.3f22fb41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f2279a3e7350efc6f27",
+    "url": "/mohammad523/the-weather-app/tree/master/morningdose/static/js/main.e1af7ccf.chunk.js"
+  },
+  {
+    "revision": "a4ec9741c6ceaeb0df41",
+    "url": "/mohammad523/the-weather-app/tree/master/morningdose/static/js/runtime-main.35962b3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mohammad523/the-weather-app/tree/master/morningdose/static/media/logo.5d5d9eef.svg"
+  }
+]);
